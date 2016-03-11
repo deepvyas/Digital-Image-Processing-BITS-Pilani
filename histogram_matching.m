b04f4a355614930cb1ca5e325a6a73d5aa597d28
Deep Vyas <vyasdeep.dv@gmail.com>
2016-03-11 20:34:44 +0530

    certain errors removed

--- histogram_matching.m
+++ histogram_matching.m
@@ -1,4 +1,4 @@
-image = imread(input('Image ro process : '));
+image = imread(input('Image to process : '));
 ref_image = imread(input('Reference image : '));
 freq_table1 = zeros(1,256);
 freq_table2 = zeros(1,256);
@@ -7,14 +7,14 @@ count2=0;
 for k=1:256
     temp1 = image(image==k-1);
     temp2 = ref_image(ref_image==k-1);
-    freq_table1 = size(temp1,1);
-    freq_table2 = size(temp2,1);
+    freq_table1(1,k) = size(temp1,1);
+    freq_table2(1,k) = size(temp2,1);
     count1 = count1+size(temp1,1);
-    count2 = count2 +size(temp2,2);
+    count2 = count2 +size(temp2,1);
 end
 
-freq_table1 = freq_table1./count1;
-freq_table2 = freq_table2./count2;
+freq_table1 = freq_table1/count1;
+freq_table2 = freq_table2/count2;
 
 cdf_image = zeros(1,256);
 cdf_ref = zeros(1,256);
@@ -28,9 +28,6 @@ output_map = zeros(1,256);
 
 for k=1:256
     temp = cdf_ref - cdf_image(1,k);
-    [m n] = min(temp);
+    [m n] = min(abs(temp));
     output_map(1,k) = n;
 end
-
-    
-    
